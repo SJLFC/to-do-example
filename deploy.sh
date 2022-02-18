7664af1122db8d9602ae5a701ad3cf1f81c448d1
SJLFC <97690027+SJLFC@users.noreply.github.com>
2022-02-18 15:32:50 +0000

    commit delpoy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -f data.db ]
+  then ./install.sh
+  echo "Successfully installed packages"
+  source .venv/bin/activate
+  python3 create.py
+  echo "Successfully created database"
+fi
+
+source .venv/bin/activate
+python3 app.py
+© 2022 GitHub, Inc.
+Terms
