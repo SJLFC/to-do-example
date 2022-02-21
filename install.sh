52d5c7da5019728d28cdc74d437fdf6adf71c550
SJLFC <97690027+SJLFC@users.noreply.github.com>
2022-02-21 11:40:35 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@ echo "Updating packages"
 sudo apt-get update
 python -m ensurepip --upgrade
 echo "installing Python"
-sudo apt install python3
+sudo apt install python3 -y
 echo "installing pip"
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 echo "creating virtual environment"
-pt install python3.8 python3.8-venv python3-venv
-python3.8 -m venv .env
+pt install python3.8 python3.8-venv python3-venv -y
+python3.8 -m venv .env -y
 source .env/bin/activate
-python --version
+python --version 
 echo "installing project requirements"
-pip install -r requirements.txt
+pip install -r requirements.txt 
